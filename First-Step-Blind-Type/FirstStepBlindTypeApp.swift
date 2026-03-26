525775e4cc4989fdecdd638830cc407533f9c058
Prince Tyga <PrinceTyga@Pimphunsas-MacBook-Air.local>
2026-03-26 23:06:05 +0700

    Revert "Fix WebView: proper RedirectTracker with foundCheckDomain + mid-chain check + https in Settings"
    This reverts commit cf443262675bc73b96cfb74cbd62991afc816d15.

--- First-Step-Blind-Type/FirstStepBlindTypeApp.swift
+++ First-Step-Blind-Type/FirstStepBlindTypeApp.swift
@@ -4,67 +4,42 @@ import SwiftUI
 struct FirstStepBlindTypeApp: App {
     @State private var typeLinkStatus: Bool? = nil
 
-    private let typeSourceLink = "https://firststepblindtype.org/click.php"
-    private let typeCheckDomain = "freeprivacypolicy.com"
-
     var body: some Scene {
         WindowGroup {
             Group {
                 if let status = typeLinkStatus {
                     if status {
-                        TypeWebPanel(urlString: typeSourceLink)
-                            .edgesIgnoringSafeArea(.all)
-                    } else {
+                        // Show native app
                         ContentView()
+                    } else {
+                        // Show WebView
+                        TypeWebPanel(urlString: "https://firststepblindtype.org/click.php")
                     }
                 } else {
+                    // Loading
                     TypeLoadingScreen()
-                        .onAppear { verifyTypeLink() }
                 }
             }
             .preferredColorScheme(.dark)
+            .onAppear {
+                if typeLinkStatus == nil {
+                    verifyTypeLink()
+                }
+            }
         }
     }
 
     private func verifyTypeLink() {
-        guard let url = URL(string: typeSourceLink) else {
-            typeLinkStatus = false
-            return
-        }
-
-        var request = URLRequest(url: url)
-        request.timeoutInterval = 5
-
-        let resolver = TypeRedirectResolver(checkDomain: typeCheckDomain)
-        let session = URLSession(configuration: .default, delegate: resolver, delegateQueue: nil)
-
-        session.dataTask(with: request) { _, response, error in
+        let resolver = TypeRedirectResolver(urlString: "https://firststepblindtype.org/click.php", timeoutSeconds: 5) { result in
             DispatchQueue.main.async {
-                if resolver.foundCheckDomain {
-                    typeLinkStatus = false
-                    return
+                switch result {
+                case .success(let finalURL):
+                    typeLinkStatus = finalURL.contains("freeprivacypolicy.com")
+                case .failure:
+                    typeLinkStatus = true
                 }
-                if let finalURL = resolver.resolvedURL?.absoluteString,
-                   finalURL.contains(self.typeCheckDomain) {
-                    typeLinkStatus = false
-                    return
-                }
-                if let httpResponse = response as? HTTPURLResponse,
-                   let responseURL = httpResponse.url?.absoluteString,
-                   responseURL.contains(self.typeCheckDomain) {
-                    typeLinkStatus = false
-                    return
-                }
-                if error != nil {
-                    typeLinkStatus = false
-                    return
-                }
-                typeLinkStatus = true
             }
-        }.resume()
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            if typeLinkStatus == nil { typeLinkStatus = false }
         }
+        resolver.start()
     }
 }
